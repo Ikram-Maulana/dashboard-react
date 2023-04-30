47c5a21e649286c9ecb006b1df8576a68d0ee885
Ikram Maulana <ikram075@ummi.ac.id>
2023-04-30 14:24:54 +0700

    Feat: Setting Up Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", ...fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from "@vitejs/plugin-react-swc";
+import Unfonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    Unfonts({
+      google: {
+        families: ["Poppins"],
+      },
+    }),
+  ],
 });
